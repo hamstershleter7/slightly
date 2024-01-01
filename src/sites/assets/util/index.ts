4c2892d8c9987fe649e837dfa2fa2b159c1e41ac
hamstershleter7 <hamster.shelter7@gmail.com>
2024-01-01 02:38:42 +0000

    Fix configuration - enhancing logs

--- src/sites/assets/util/index.ts
+++ src/sites/assets/util/index.ts
@@ -1,4 +1,4 @@
-let ua = navigator.userAgent.toLowerCase()
-let isMobile = /ios|iphone|ipod|ipad|android/.test(ua)
+const ua = navigator.userAgent.toLowerCase()
+const isMobile = /ios|iphone|ipod|ipad|android/.test(ua)
 
 export { isMobile }
