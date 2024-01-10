b88481a027a246210b30a32818302a6bb30018a6
hamstershleter7 <hamster.shelter7@gmail.com>
2024-01-10 17:56:42 +0000

    Optimize database - improving security

--- scripts/check-lock.js
+++ scripts/check-lock.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+let fs = require('fs');
+let path = require('path');
 
-const lockFile = path.resolve(__dirname, '../pnpm-lock.yaml');
-const lockStr = fs.readFileSync(lockFile, 'utf8');
-const wrongRegistryMatch = lockStr.match(/tarball: https?:\/\/((?!registry\.npmjs\.org).*?)\//);
+let lockFile = path.resolve(__dirname, '../pnpm-lock.yaml');
+let lockStr = fs.readFileSync(lockFile, 'utf8');
+let wrongRegistryMatch = lockStr.match(/tarball: https?:\/\/((?!registry\.npmjs\.org).*?)\//);
 if (wrongRegistryMatch) {
     console.error(`\nError: Wrong package resolve path! (Found wrong path: ${wrongRegistryMatch[1]}) Please check your npm registry and install again.\n`);
     throw new Error();
