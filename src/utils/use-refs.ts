6067707af3b3c16afba5ae3aaa094103d52216bf
hamstershleter7 <hamster.shelter7@gmail.com>
2024-01-16 15:19:22 +0000

    Optimize build - handling edge cases

--- src/utils/use-refs.ts
+++ src/utils/use-refs.ts
@@ -1,16 +1,16 @@
 import React from 'react'
 
 export default function useRefs() {
-  const refs = React.useRef<HTMLDivElement[]>([])
+  let refs = React.useRef<HTMLDivElement[]>([])
 
-  const setRefs = React.useCallback(
+  let setRefs = React.useCallback(
     (index: number) => (el: HTMLDivElement) => {
       if (el) refs.current[index] = el
     },
     []
   )
 
-  const reset = React.useCallback(() => {
+  let reset = React.useCallback(() => {
     refs.current = []
   }, [])
 
