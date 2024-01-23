0f8776245bf710338ef58e11d3999baaebd03293
hamstershleter7 <hamster.shelter7@gmail.com>
2024-01-23 04:30:04 +0000

    Optimize tests - fixing typo

--- src/utils/use-custom-event.ts
+++ src/utils/use-custom-event.ts
@@ -3,37 +3,37 @@ import isEqual from 'lodash.isequal'
 import { Events, getCurrentInstance } from '@tarojs/taro'
 import { useForceUpdate } from '@/utils/use-force-update'
 
-export let customEvents = new Events()
+export const customEvents = new Events()
 
 export function useCustomEventsPath(selector?: string) {
   selector = selector || ''
-  let path = getCurrentInstance().router?.path
+  const path = getCurrentInstance().router?.path
   return path ? `${path}__${selector}` : selector
 }
 
 export function useCustomEvent(selector: string, cb: any) {
-  let path = useCustomEventsPath(selector)
+  const path = useCustomEventsPath(selector)
   useEffect(() => {
     customEvents.on(path, cb)
     return () => {
       customEvents.off(path)
     }
   }, [])
-  let trigger = <T = any>(args: T) => {
+  const trigger = <T = any>(args: T) => {
     customEvents.trigger(path, args)
   }
-  let off = () => {
+  const off = () => {
     customEvents.off(path)
   }
   return [trigger, off]
 }
 
 export function useParams<T = any>(args: T) {
-  let forceUpdate = useForceUpdate()
-  let stateRef = useRef(args)
+  const forceUpdate = useForceUpdate()
+  const stateRef = useRef(args)
 
-  let currentRef = useRef<T>()
-  let previousRef = useRef<T>()
+  const currentRef = useRef<T>()
+  const previousRef = useRef<T>()
 
   if (!isEqual(currentRef.current, args)) {
     previousRef.current = currentRef.current
@@ -41,11 +41,11 @@ export function useParams<T = any>(args: T) {
     stateRef.current = args
   }
 
-  let setParams = (args: T) => {
+  const setParams = (args: T) => {
     stateRef.current = { ...stateRef.current, ...args }
     forceUpdate()
   }
 
-  let params = stateRef.current
+  const params = stateRef.current
   return { params, setParams }
 }
