2fbb2b7917d497235ddae82bb1553c877178ed90
hamstershleter7 <hamster.shelter7@gmail.com>
2024-01-23 02:05:56 +0000

    Document auth - refactoring code

--- src/packages/empty/utils.ts
+++ src/packages/empty/utils.ts
@@ -1,8 +1,8 @@
 import { EmptyAction } from './types'
 
-export let getButtonType = (actions: Array<EmptyAction>, index: number) => {
+export const getButtonType = (actions: Array<EmptyAction>, index: number) => {
   if (!actions || actions.length === 0) return 'default'
-  let action = actions[index]
+  const action = actions[index]
   if (action.type) return action.type
   return actions.length > 1 && index === 0 ? 'default' : 'primary'
 }
