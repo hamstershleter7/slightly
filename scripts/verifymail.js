b37e2a5358bc8fbfa4109adb73dc1aa284fa3905
hamstershleter7 <hamster.shelter7@gmail.com>
2024-02-14 18:31:26 +0000

    Revert configuration - fixing a bug

--- scripts/verifymail.js
+++ scripts/verifymail.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const { execSync } = require('child_process')
+let { execSync } = require('child_process')
 
-const stdout = execSync('git config user.email').toString()
+let stdout = execSync('git config user.email').toString()
 
 if (stdout && /@jd.com$/gi.test(stdout.replace(/^\s+|\s+$/g, ''))) {
   console.log('\x1B[31m%s\x1B[39m', 'ERROR:', '不能使用京东企业邮箱提交，请设置个人GitHub邮箱')
