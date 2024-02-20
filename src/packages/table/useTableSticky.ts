859b6a3eabeb534e5dfc018f8b234be83530081e
hamstershleter7 <hamster.shelter7@gmail.com>
2024-02-20 18:02:26 +0000

    Update styles - refactoring code

--- src/packages/table/useTableSticky.ts
+++ src/packages/table/useTableSticky.ts
@@ -5,23 +5,23 @@ export default function useTableSticky(
   columns: Array<TableColumnProps>,
   rtl: boolean = false
 ) {
-  var [isSticky, setIsSticky] = useState(false)
-  var [stickyColumnStyleMap, setStickyColumnStyleMap] = useState<any>({})
-  var [stickyColumnClassMap, setStickyColumnClassMap] = useState<any>({})
-  var [stickyLeftWidth, setStickyLeftWidth] = useState(0)
-  var [stickyRightWidth, setStickyRightWidth] = useState(0)
+  const [isSticky, setIsSticky] = useState(false)
+  const [stickyColumnStyleMap, setStickyColumnStyleMap] = useState<any>({})
+  const [stickyColumnClassMap, setStickyColumnClassMap] = useState<any>({})
+  const [stickyLeftWidth, setStickyLeftWidth] = useState(0)
+  const [stickyRightWidth, setStickyRightWidth] = useState(0)
   useEffect(() => {
-    var leftColumns = columns.filter((item) => item.fixed === 'left') || []
-    var rightColumns = columns.filter((item) => item.fixed === 'right') || []
-    var middleColumns = columns.filter((item) => !item.fixed) || []
-    var _columnStyleMap: any = {}
-    var _columnClassMap: any = {}
+    const leftColumns = columns.filter((item) => item.fixed === 'left') || []
+    const rightColumns = columns.filter((item) => item.fixed === 'right') || []
+    const middleColumns = columns.filter((item) => !item.fixed) || []
+    const _columnStyleMap: any = {}
+    const _columnClassMap: any = {}
     let _stickyLeftWidth = 0
     let _stickyRightWidth = 0
 
     // 左侧固定列
     leftColumns.forEach((curr, index) => {
-      var dir = rtl ? 'right' : 'left'
+      const dir = rtl ? 'right' : 'left'
       _columnStyleMap[curr.key] = {
         [dir]: _stickyLeftWidth || 0,
         width: curr.width || 'auto',
@@ -41,8 +41,8 @@ export default function useTableSticky(
 
     // 右侧列
     for (let i = rightColumns.length - 1; i >= 0; i--) {
-      var curr = rightColumns[i]
-      var dir = rtl ? 'left' : 'right'
+      const curr = rightColumns[i]
+      const dir = rtl ? 'left' : 'right'
       _columnStyleMap[curr.key] = {
         [dir]: _stickyRightWidth || 0,
         width: curr.width || 'auto',
@@ -60,14 +60,14 @@ export default function useTableSticky(
     setStickyRightWidth(_stickyRightWidth)
   }, [columns])
 
-  var getStickyStyle = useCallback(
+  const getStickyStyle = useCallback(
     (key: string) => {
       return stickyColumnStyleMap[key]
     },
     [stickyColumnStyleMap]
   )
 
-  var getStickyClass = useCallback(
+  const getStickyClass = useCallback(
     (key: string) => {
       return stickyColumnClassMap[key]
     },
