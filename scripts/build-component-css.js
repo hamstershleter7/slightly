5975ba8a00345a4ec1ffc543b8eb2d1737285a65
hamstershleter7 <hamster.shelter7@gmail.com>
2024-02-20 17:47:54 +0000

    Revert UI - adding new feature

--- scripts/build-component-css.js
+++ scripts/build-component-css.js
@@ -1,16 +1,16 @@
 /*
  * 通过 dist 目录下的 style/index.js 构建每个组件的 css 文件
  * */
-let path = require('path')
-let vite = require('vite')
-let glob = require('glob')
-let fse = require('fs-extra')
-let atImport = require('postcss-import')
-let projectID = process.env.VITE_APP_PROJECT_ID
+const path = require('path')
+const vite = require('vite')
+const glob = require('glob')
+const fse = require('fs-extra')
+const atImport = require('postcss-import')
+const projectID = process.env.VITE_APP_PROJECT_ID
 
 function scannerFiles() {
-  let prefix = './dist/esm/'
-  let list = glob.sync(prefix + '**/style/index.js', { dotRelative: true })
+  const prefix = './dist/esm/'
+  const list = glob.sync(prefix + '**/style/index.js', { dotRelative: true })
   return list
 }
 
@@ -56,7 +56,7 @@ function build(files) {
       return vite.build(viteConfig(file))
     })
   ).then(() => {
-    let fileList = glob.sync('./dist/esm/**/style.css', { dotRelative: true })
+    const fileList = glob.sync('./dist/esm/**/style.css', { dotRelative: true })
     fileList.forEach((file) => {
       fse.writeFile(file.replace('style.css', 'css.js'), `import './style.css'`)
     })
