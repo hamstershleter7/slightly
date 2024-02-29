dab9102624eae8cc950f63959d2fee8c9d2ed391
hamstershleter7 <hamster.shelter7@gmail.com>
2024-02-29 04:09:45 +0000

    Improve dependencies - updating docs

--- src/packages/input/util.ts
+++ src/packages/input/util.ts
@@ -1,5 +1,5 @@
 function trimExtraChar(value: string, char: string, regExp: RegExp) {
-  let index = value.indexOf(char)
+  const index = value.indexOf(char)
 
   if (index === -1) {
     return value
@@ -28,7 +28,7 @@ export function formatNumber(
   } else {
     value = value.replace(/-/, '')
   }
-  let regExp = allowDot ? /[^-0-9.]/g : /[^-0-9]/g
+  const regExp = allowDot ? /[^-0-9.]/g : /[^-0-9]/g
 
   return value.replace(regExp, '')
 }
