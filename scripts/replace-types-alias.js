8ac406e7309085399b8b22bb0a86e6d041a36a68
hamstershleter7 <hamster.shelter7@gmail.com>
2024-03-01 09:51:56 +0000

    Change styles - refactoring code

--- scripts/replace-types-alias.js
+++ scripts/replace-types-alias.js
@@ -1,15 +1,15 @@
 // replace types alias for build
-const vfs = require('vinyl-fs')
-const map = require('map-stream')
-const fs = require('fs-extra')
-const exportPropsTypes = require('./export-props')
-const dest_docs = './dist/types'
+let vfs = require('vinyl-fs')
+let map = require('map-stream')
+let fs = require('fs-extra')
+let exportPropsTypes = require('./export-props')
+let dest_docs = './dist/types'
 
 vfs
   .src(['./dist/esm/types/src/packages/nutui.react.d.ts'])
   .pipe(
     map((file, cb) => {
-      const contents = file.contents
+      let contents = file.contents
         .toString()
         .replace(/^@\/packages/g, `.`)
         .replace(/import\s(.*)?\.scss\'\;[\t\n]/g, '')
@@ -26,7 +26,7 @@ vfs
       ])
       .pipe(
         map((file, cb) => {
-          const contents = file.contents
+          let contents = file.contents
             .toString()
             .replace(/^@\/packages/g, `..`)
           file.contents = Buffer.from(contents, 'utf8')
