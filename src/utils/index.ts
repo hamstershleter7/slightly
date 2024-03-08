b6a718c709fbd7624ffeab5b9099b4b18e09b946
hamstershleter7 <hamster.shelter7@gmail.com>
2024-03-08 14:31:32 +0000

    Change UI - refactoring code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,15 @@
-export let isObject = (val: unknown): val is Record<any, any> =>
+export const isObject = (val: unknown): val is Record<any, any> =>
   val !== null && typeof val === 'object'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export let isFunction = (val: unknown): val is Function =>
+export const isFunction = (val: unknown): val is Function =>
   typeof val === 'function'
 
-export let isPromise = <T = any>(val: unknown): val is Promise<T> => {
+export const isPromise = <T = any>(val: unknown): val is Promise<T> => {
   return isObject(val) && isFunction(val.then) && isFunction(val.catch)
 }
 
-export let upperCaseFirst = (str: string) => {
+export const upperCaseFirst = (str: string) => {
   str = str.toLowerCase()
   str = str.replace(
     /\b\w+\b/g,
@@ -18,7 +18,7 @@ export let upperCaseFirst = (str: string) => {
   return str
 }
 
-export let clamp = (num: number, min: number, max: number): number =>
+export const clamp = (num: number, min: number, max: number): number =>
   Math.min(Math.max(num, min), max)
 
 export function preventDefault(
