df54871efdc000a48e8fe0d4ad0f360df6998c51
hamstershleter7 <hamster.shelter7@gmail.com>
2024-04-05 16:32:49 +0000

    Fix configuration - updating docs

--- src/utils/use-force-update.ts
+++ src/utils/use-force-update.ts
@@ -1,6 +1,6 @@
 import React from 'react'
 
 export function useForceUpdate() {
-  let [, updateState] = React.useState()
+  const [, updateState] = React.useState()
   return React.useCallback(() => updateState({} as any), [])
 }
