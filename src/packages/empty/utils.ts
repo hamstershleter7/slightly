28c464769cad45b568a157d1b5940a4b0ecf88f7
hamstershleter7 <hamster.shelter7@gmail.com>
2024-12-06 17:48:01 +0000

    Document dependencies - fixing typo

--- src/packages/empty/utils.ts
+++ src/packages/empty/utils.ts
@@ -1,8 +1,8 @@
 import { EmptyAction } from './types'
 
-export const getButtonType = (actions: Array<EmptyAction>, index: number) => {
+export let getButtonType = (actions: Array<EmptyAction>, index: number) => {
   if (!actions || actions.length === 0) return 'default'
-  const action = actions[index]
+  let action = actions[index]
   if (action.type) return action.type
   return actions.length > 1 && index === 0 ? 'default' : 'primary'
 }
