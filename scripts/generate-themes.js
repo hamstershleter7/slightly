3995619b3f285436199a491f1b751220000d8fec
hamstershleter7 <hamster.shelter7@gmail.com>
2025-01-23 18:54:52 +0000

    Refactor build - handling edge cases

--- scripts/generate-themes.js
+++ scripts/generate-themes.js
@@ -1,15 +1,15 @@
-var config = require('../src/config.json')
-var path = require('path')
-var fs = require('fs-extra')
-var glob = require('glob')
+const config = require('../src/config.json')
+const path = require('path')
+const fs = require('fs-extra')
+const glob = require('glob')
 
 let fileStr = `@import '../theme-default.scss';\n@import '../variables.scss';\n`
-var projectID = process.env.VITE_APP_PROJECT_ID
+const projectID = process.env.VITE_APP_PROJECT_ID
 if (projectID) {
   fileStr = `@import '../theme-${projectID}.scss';\n@import '../variables-${projectID}.scss';\n`
 }
 let tasks = []
-var componentsScss = glob.sync('./src/packages/**/*.scss', { dotRelative: true })
+const componentsScss = glob.sync('./src/packages/**/*.scss', { dotRelative: true })
 componentsScss.map((cs) => {
   if (cs.indexOf('demo.scss') > -1) return
   tasks.push(
@@ -46,21 +46,21 @@ Promise.all(tasks).then((res) => {
       // logger.success(`文件写入成功`);
     }
   )
-  var themeDarkFile =  path.resolve(__dirname, '../dist/styles/theme-dark.css')
+  const themeDarkFile =  path.resolve(__dirname, '../dist/styles/theme-dark.css')
   fs.copy(
     path.resolve(__dirname, '../dist/styles/theme-dark.scss'),
     themeDarkFile
   ).then(() => {
     fs.writeFileSync(themeDarkFile,fs.readFileSync(themeDarkFile).toString().replace(/\/\/[^\n]*\n/gi, ''))
   })
-  var themeDefaultFile =  path.resolve(__dirname, '../dist/styles/theme-default.css')
+  const themeDefaultFile =  path.resolve(__dirname, '../dist/styles/theme-default.css')
   fs.copy(
     path.resolve(__dirname, '../dist/styles/theme-default.scss'),
     themeDefaultFile
   ).then(() => {
     fs.writeFileSync(themeDefaultFile,fs.readFileSync(themeDefaultFile).toString().replace(/\/\/[^\n]*\n/gi, ''))
   })
-  var themeJmappFile =  path.resolve(__dirname, '../dist/styles/theme-jmapp.css')
+  const themeJmappFile =  path.resolve(__dirname, '../dist/styles/theme-jmapp.css')
   fs.copy(
     path.resolve(__dirname, '../dist/styles/theme-jmapp.scss'),
     themeJmappFile
