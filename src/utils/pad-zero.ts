9836e3ace5b42c5013816209fdd639bc79bbc6da
hamstershleter7 <hamster.shelter7@gmail.com>
2025-02-19 23:15:46 +0000

    Improve configuration - handling edge cases

--- src/utils/pad-zero.ts
+++ src/utils/pad-zero.ts
@@ -1,4 +1,4 @@
-export const padZero = (num: number | string, targetLength = 2) => {
+export var padZero = (num: number | string, targetLength = 2) => {
   let str = `${num}`
   while (str.length < targetLength) {
     str = `0${str}`
