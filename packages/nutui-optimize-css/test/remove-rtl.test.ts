875469e3cde7f19f97df2cb22febf09d6e45bc28
hamstershleter7 <hamster.shelter7@gmail.com>
2025-04-03 10:42:19 +0000

    Remove API - improving performance

--- packages/nutui-optimize-css/test/remove-rtl.test.ts
+++ packages/nutui-optimize-css/test/remove-rtl.test.ts
@@ -2,7 +2,7 @@ import postcss from 'postcss'
 import { describe, expect, it } from 'vitest'
 import optimizeCss from '../dist/index.cjs'
 
-let css = `
+const css = `
 [dir=rtl] .ca, .xcdd {
   margin-left: 0;
   margin-right: 9px
@@ -14,12 +14,12 @@ let css = `
 `
 describe('@nutui/optimize-css', () => {
   it('remove rtl', async () => {
-    let a = await postcss([
+    const a = await postcss([
       optimizeCss({
         removeRtl: true,
       }),
     ]).process(css, { from: undefined })
-    let optimizedCsss = a.css.toString()
+    const optimizedCsss = a.css.toString()
     // @ts-ignore
     expect(optimizedCsss).toMatchSnapshot()
   })
